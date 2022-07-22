0029e75bf2aae9076ae252021a981ae9dbf6cb25
cosmo <zhixuan0309@gmail.com>
2022-07-22 14:57:49 +0800

    fixed test some

--- src/mysql_demo/conn_demo.rs
+++ src/mysql_demo/conn_demo.rs
@@ -13,10 +13,26 @@ pub fn mysql_conn_demo() -> std::result::Result<(), Box<dyn std::error::Error>> 
     let pool = Pool::new(url)?;
     let mut conn = pool.get_conn()?;
     #[allow(unused_parens)]
-    let selected_test = conn.query_map(
-        "SELECT ID id, NAME name FROM TEST",
+    let selected_test = match conn.query_map(
+        "SELECT ID id, NAME name FROM TEST1",
         | ( id, name) | { Test { id, name } },
-    )?;
+    ) {
+        Ok(val) => val,
+        Err(_) => {
+            println!("Fail");
+            Vec::new()
+        }
+    };
+    // let selected_test = conn.query_map(
+    //     "SELECT ID id, NAME name FROM TEST1",
+    //     | ( id, name) | { Test { id, name } },
+    // ).expect("Fail query");
+
+    // let selected_test = conn.query_map(
+    //     "SELECT ID id, NAME name FROM TEST1",
+    //     | ( id, name) | { Test { id, name } },
+    // )?;
+
     for (i, select) in selected_test .iter().enumerate(){
         println!("index: {}, value(id): {}, value(name): {}", i, &select.id, &select.name);
     }
